d7913aab1c4d20bbf9079ad055e7810d9466a8ab
Sukhada Bhoyar <sukhadabhoyar@gmail.com>
2025-04-10 20:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if uploaded_file is not None:
     st.image(img, caption='Uploaded Image', use_column_width=True)
 
     # Preprocess the image (update size if your model expects a different input shape)
-    img = img.resize((128, 128))  # Change size according to model input
+    img = img.resize((150, 150))  # Change size according to model input
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize
 
