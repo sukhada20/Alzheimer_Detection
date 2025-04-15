fa2746902706dd0c7561387ac86d8573a44d43ae
Sukhada Bhoyar <sukhadabhoyar@gmail.com>
2025-04-15 14:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,16 +19,14 @@ uploaded_file = st.file_uploader("Upload a brain MRI scan image", type=["jpg", "
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert('RGB')
-    st.image(img, caption='Uploaded Image', use_column_width=True)
+    st.image(img, caption='Uploaded Image', use_container_width=True)
 
-    # Preprocess the image
     st.write("Processing the image...")
     img_resized = img.resize((150, 150)) 
     img_array = image.img_to_array(img_resized)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalizing
+    img_array /= 255.0  
 
-    # Make prediction
     prediction = model.predict(img_array)
     predicted_class = final_labels[np.argmax(prediction)]
 
@@ -38,4 +36,4 @@ if uploaded_file is not None:
         st.write(f"{label}: {prediction[0][i]:.4f}")
 
 st.markdown("---")
-st.info("This tool uses deep learning to assist with early detection of Alzheimer’s Disease. Not for clinical use.")
+st.info("This tool uses deep learning to assist with early detection of Alzheimer’s Disease.")
